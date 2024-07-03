6ca2fb008d4c62f1c12ceb424cc0a5989bc31237
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-03 17:53:24 +0300

    feat: add editContact

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,11 +1,10 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
-import axios from "axios";
-
-axios.defaults.baseURL = "https://667d183e297972455f637047.mockapi.io";
+import {goitApi} from "../../config/goitApi";
+import toast from "react-hot-toast";
 
 export const fetchContacts = createAsyncThunk("contacts/FetchAll", async (_, thunkAPI) => {
   try {
-    const {data} = await axios.get("/contacts");
+    const {data} = await goitApi.get("/contacts");
     return data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
@@ -14,7 +13,8 @@ export const fetchContacts = createAsyncThunk("contacts/FetchAll", async (_, thu
 
 export const addContact = createAsyncThunk("contacts/addContact", async (contact, thunkAPI) => {
   try {
-    const {data} = await axios.post("/contacts", contact);
+    const {data} = await goitApi.post("/contacts", contact);
+    toast.success("Contact was successfully added!");
     return data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
@@ -23,8 +23,19 @@ export const addContact = createAsyncThunk("contacts/addContact", async (contact
 
 export const deleteContact = createAsyncThunk("contacts/deleteContact", async (id, thunkAPI) => {
   try {
-    const {data} = await axios.delete(`/contacts/${id}`);
+    const {data} = await goitApi.delete(`/contacts/${id}`);
+    toast.success("Contact was successfully deleted!");
     return data.id;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+export const editContact = createAsyncThunk("contacts/editContact", async (contact, thunkAPI) => {
+  console.log(contact.id);
+  try {
+    const {data} = await goitApi.patch(`/contacts/${contact.id}`, contact);
+    return data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
   }
