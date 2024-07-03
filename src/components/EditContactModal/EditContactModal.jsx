91caffec85f65ca1c6991294928c817e061ad65c
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-03 18:58:30 +0300

    feat: styles modal windows

--- src/components/EditContactModal/EditContactModal.jsx
+++ src/components/EditContactModal/EditContactModal.jsx
@@ -25,59 +25,60 @@ const EditContactModal = ({isOpen, onClose}) => {
     <Modal isOpen={isOpen} onRequestClose={onClose} style={customStyles}>
       {currentContact && (
         <>
-          <button className="absolute right-4 top-4" onClick={onClose}>
-            <GrClose />
-          </button>
-
-          <h1 className="text-center mb-4 text-lg font-bold">Edit Contact</h1>
-          <Formik
-            initialValues={{
-              nameContact: currentContact.name,
-              numberContact: currentContact.number,
-            }}
-            onSubmit={handleSubmitModal}
-            validationSchema={FeedbackSchema}
-          >
-            {({setFieldValue}) => {
-              return (
-                <Form className="flex flex-col gap-2">
-                  <div>
-                    <label htmlFor="name" className="px-4 text-xs">
-                      Name
-                    </label>
-                    <Field
-                      id="name"
-                      name="nameContact"
-                      type="text"
-                      className="input input-bordered"
-                    />
-                    <ErrorMessage name="name" component="div" />
-                  </div>
-                  <div>
-                    <label htmlFor="number" className="px-4 text-xs">
-                      Number
-                    </label>
-                    <Field name="numberContact">
-                      {({field}) => (
-                        <ReactInputMask
-                          {...field}
-                          mask="999-99-99"
-                          maskChar="_"
-                          placeholder="___-__-__"
-                          onChange={(e) => setFieldValue("number", e.target.value)}
-                          className="input input-bordered"
-                        />
-                      )}
-                    </Field>
-                    <ErrorMessage name="number" component="div" />
-                  </div>
-                  <button type="submit" className="btn btn-outline">
-                    Save
-                  </button>
-                </Form>
-              );
-            }}
-          </Formik>
+          <div className="modal-box">
+            <button className="absolute right-4 top-4" onClick={onClose}>
+              <GrClose />
+            </button>
+            <h1 className="text-center mb-4 text-lg font-bold">Edit Contact</h1>
+            <Formik
+              initialValues={{
+                nameContact: currentContact.name,
+                numberContact: currentContact.number,
+              }}
+              onSubmit={handleSubmitModal}
+              validationSchema={FeedbackSchema}
+            >
+              {({setFieldValue}) => {
+                return (
+                  <Form className="flex flex-col gap-3">
+                    <div>
+                      <label htmlFor="name" className="px-4 text-xs">
+                        Name
+                      </label>
+                      <Field
+                        id="name"
+                        name="nameContact"
+                        type="text"
+                        className="input input-bordered"
+                      />
+                      <ErrorMessage name="name" component="div" />
+                    </div>
+                    <div>
+                      <label htmlFor="number" className="px-4 text-xs">
+                        Number
+                      </label>
+                      <Field name="numberContact">
+                        {({field}) => (
+                          <ReactInputMask
+                            {...field}
+                            mask="999-99-99"
+                            maskChar="_"
+                            placeholder="___-__-__"
+                            onChange={(e) => setFieldValue("number", e.target.value)}
+                            className="input input-bordered"
+                          />
+                        )}
+                      </Field>
+                      <ErrorMessage name="number" component="div" />
+                    </div>
+                    <button type="submit" className="btn btn-outline">
+                      Save
+                    </button>
+                  </Form>
+                );
+              }}
+            </Formik>
+          </div>
         </>
       )}
     </Modal>
--- src/components/NotificationDelete/NotificationDelete.jsx
+++ src/components/NotificationDelete/NotificationDelete.jsx
@@ -13,14 +13,16 @@ const NotificationDelete = ({isOpenWindow, onClose, id}) => {
   };
   return (
     <Modal isOpen={isOpenWindow} onRequestClose={onClose} style={customStyles}>
-      <p className="text-xl text-center mb-4">Do you want delete this contact?</p>
-      <div className="flex justify-center gap-4">
-        <button className="btn btn-outline btn-sm" onClick={handleDelete}>
-          Yes
-        </button>
-        <button className="btn btn-outline btn-sm" onClick={onClose}>
-          No
-        </button>
+      <div className="modal-box">
+        <p className="text-xl text-center mb-4">Do you want delete this contact?</p>
+        <div className="flex justify-center gap-4">
+          <button className="btn btn-outline btn-sm" onClick={handleDelete}>
+            Yes
+          </button>
+          <button className="btn btn-outline btn-sm" onClick={onClose}>
+            No
+          </button>
+        </div>
       </div>
     </Modal>
   );
