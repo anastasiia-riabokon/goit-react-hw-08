73903a14d3299bcfd35196dff861f24914a95b23
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-03 09:20:36 +0300

    feat: feedback on register

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,11 +1,17 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
 import {goitApi} from "../../config/goitApi";
+import toast from "react-hot-toast";
 
 export const registerThunk = createAsyncThunk("auth/register", async (credential, thunkAPI) => {
   try {
     const {data} = await goitApi.post("/users/signup", credential);
+    toast.success("Registration successful!");
+
     return data;
   } catch (error) {
-    return thunkAPI.rejectWithValue(error.message);
+    if (error.response && error.response.data && error.response.data.code === 11000) {
+      return toast.error("An account with this email already exists.");
+    }
+    return rejectWithValue(error.message);
   }
 });
