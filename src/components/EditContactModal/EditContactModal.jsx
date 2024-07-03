a787dc85c5fe5f125998d24ddd5944d35e322bb0
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-03 18:15:39 +0300

    feat: custom styles for modal windows in separate file

--- src/components/EditContactModal/EditContactModal.jsx
+++ src/components/EditContactModal/EditContactModal.jsx
@@ -7,26 +7,7 @@ import {FeedbackSchema} from "../../helpers/FeedbackSchema";
 import ReactInputMask from "react-input-mask";
 import {setCurrentContact} from "../../redux/contacts/slice";
 import {GrClose} from "react-icons/gr";
-
-const customStyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-    width: "70%",
-    padding: "24px",
-    paddingTop: "32px",
-    borderRadius: "12px",
-    boxShadow: "0 25px 50px rgba(0, 0, 0, 0.5), 0 10px 20px rgba(0, 0, 0, 0.3)",
-    maxWidth: "350px",
-  },
-  overlay: {
-    backgroundColor: "rgba(40, 40, 40, 0.75)",
-  },
-};
+import {customStyles} from "../../helpers/customStyleForModal";
 
 Modal.setAppElement("#root");
 
---
+++ src/helpers/customStyleForModal.jsx
@@ -0,0 +1,19 @@
+export const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    width: "70%",
+    padding: "24px",
+    paddingTop: "32px",
+    borderRadius: "12px",
+    boxShadow: "0 25px 50px rgba(0, 0, 0, 0.5), 0 10px 20px rgba(0, 0, 0, 0.3)",
+    maxWidth: "350px",
+  },
+  overlay: {
+    backgroundColor: "rgba(40, 40, 40, 0.75)",
+  },
+};
