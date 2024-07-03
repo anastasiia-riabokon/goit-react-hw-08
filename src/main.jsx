dbf65f9352d03ce4df2d3aef850c2aa743eb7e2c
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-03 09:19:08 +0300

    feat: add Toaster

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import "./index.css";
 import {store} from "./redux/store";
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
+import {Toaster} from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
+      <Toaster position="top-center" reverseOrder={false} />
     </BrowserRouter>
   </Provider>
 );
