2cc8df78f734dc2cb2df5fbcf7b2b42804650f40
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-03 07:07:30 +0300

    feat: init routes settings

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,22 @@
-import {useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
-
-import ContactList from "./ContactList/ContactList";
-import SearchBox from "./SearchBox/SearchBox";
-import ContactForm from "./ContactForm/ContactForm";
-import Notification from "./Notification/Notification";
-import Title from "./Title/Title";
-import Section from "./Layout/Section";
-import Container from "./Layout/Container";
-import Loader from "./Loader/Loader";
-
-import {selectFilteredContacts, selectNameFilter} from "../redux/filters/selectors";
-import {selectContacts, selectError, selectLoading} from "../redux/contacts/selectors";
-import {fetchContacts} from "../redux/contacts/operations";
+import {Route, Routes} from "react-router-dom";
+import Layout from "./Layout/Layout";
+import HomePage from "../pages/HomePage/HomePage";
+import ContactsPage from "../pages/ContactsPage/ContactsPage";
+import LoginPage from "../pages/LoginPage/LoginPage";
+import RegistrationPage from "../pages/RegistrationPage/RegistrationPage";
 
 function App() {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectNameFilter);
-  const isLoading = useSelector(selectLoading);
-  const isError = useSelector(selectError);
-  const filteredContacts = useSelector(selectFilteredContacts);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
-
   return (
-    <Section>
-      <Container>
-        <Title />
-        <ContactForm />
-        <SearchBox />
-        {isLoading && <Loader />}
-        {!isLoading && !isError && filteredContacts.length !== 0 && (
-          <ContactList users={filteredContacts} />
-        )}
-        {filteredContacts.length === 0 && contacts.length !== 0 && (
-          <Notification text={`No contact found ${filter}`} />
-        )}
-
-        {isError && <Notification text={"Woops! Something went wrong😰"} />}
-      </Container>
-    </Section>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="contacts" element={<ContactsPage />} />
+        </Route>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegistrationPage />} />
+      </Routes>
+    </>
   );
 }
 
