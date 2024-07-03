1c554da1641025cfdcfd224f524e412397c022df
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-03 11:04:23 +0300

    feat: temporary routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,17 +4,40 @@ import HomePage from "../pages/HomePage/HomePage";
 import ContactsPage from "../pages/ContactsPage/ContactsPage";
 import LoginPage from "../pages/LoginPage/LoginPage";
 import RegistrationPage from "../pages/RegistrationPage/RegistrationPage";
+import PrivateRoute from "../routes/PrivateRoute";
+import RestrictedRoute from "../routes/RestrictedRoute";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Layout />
+            </PrivateRoute>
+          }
+        >
           <Route index element={<HomePage />} />
           <Route path="contacts" element={<ContactsPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegistrationPage />} />
         </Route>
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute>
+              <LoginPage />
+            </RestrictedRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute>
+              <RegistrationPage />
+            </RestrictedRoute>
+          }
+        />
       </Routes>
     </>
   );
