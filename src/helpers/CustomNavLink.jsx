45202bc3d7dd00dfa6d2c5b911eee8214cb11827
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-03 07:48:17 +0300

    fix: change name props

--- src/helpers/CustomNavLink.jsx
+++ src/helpers/CustomNavLink.jsx
@@ -1,13 +1,13 @@
 import clsx from "clsx";
 import {NavLink} from "react-router-dom";
 
-const CustomNavLink = ({path, children}) => {
+const CustomNavLink = ({to, children}) => {
   const buildLinkClass = ({isActive}) => {
     return clsx("hover:text-red-600", isActive && "active");
   };
 
   return (
-    <NavLink to={path} className={buildLinkClass}>
+    <NavLink to={to} className={buildLinkClass}>
       {children}
     </NavLink>
   );
