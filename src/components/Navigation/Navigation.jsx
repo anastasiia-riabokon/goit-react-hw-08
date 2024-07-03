375a319d871a71c53b36faf9bc011d5c4b916590
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-03 08:23:59 +0300

    feat: change of location svg theme

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,9 +1,11 @@
 import CustomNavLink from "../../helpers/CustomNavLink";
+import Theme from "../Theme/Theme";
 
 const Navigation = () => {
   return (
-    <li className="navbar-start">
+    <li className="navbar-start flex gap-2 items-center">
       <CustomNavLink to="/">Home</CustomNavLink>
+      <Theme />
     </li>
   );
 };
