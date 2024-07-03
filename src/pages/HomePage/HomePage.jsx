065001d08ed529c63901303b4b8e5e9f1e8e3040
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-03 12:03:15 +0300

    feat: home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,30 @@
+import {useSelector} from "react-redux";
+import {selectUser} from "../../redux/auth/selectors";
+import {Link} from "react-router-dom";
+
 const HomePage = () => {
-  return <div></div>;
+  const user = useSelector(selectUser);
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: "url(/bg.jpg)",
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-neutral-content text-center">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Hello {user.name}</h1>
+          <p className="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+            exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+          </p>
+          <Link to="/contacts" className="btn btn-outline">
+            Get Started
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default HomePage;
