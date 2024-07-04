8651b1671f82a31a110cc2dc202145ba5f7d3551
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-04 12:30:57 +0300

    refactor: change location

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -3,7 +3,7 @@ import CustomNavLink from "../../helpers/CustomNavLink";
 const AuthNav = () => {
   return (
     <>
-      <li className="navbar-center space-x-2">
+      <li className="navbar-end space-x-2">
         <CustomNavLink to="/login">Login</CustomNavLink>
         <CustomNavLink to="/register">Register</CustomNavLink>
       </li>
